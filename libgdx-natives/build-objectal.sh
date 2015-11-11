ef0509ca55919416337ba004016eee5ecc6129ac
bugvm <bug@bugvm.com>
2015-11-11 05:09:57 -0800

    bugvm

--- libgdx-natives/build-objectal.sh
+++ libgdx-natives/build-objectal.sh
@@ -12,7 +12,7 @@ cp build/Release-iphonesimulator/libObjectAl.a build/libObjectAl.a.i386
 xcodebuild -arch x86_64 -sdk iphonesimulator
 cp build/Release-iphonesimulator/libObjectAl.a build/libObjectAl.a.x86_64
 lipo build/libObjectAL.a.armv7 build/libObjectAL.a.arm64 build/libObjectAL.a.i386 build/libObjectAL.a.x86_64 -create -output build/libObjectAL.a
-cp build/libObjectAL.a ../../
+cp build/libObjectAL.a ../../libs/ios32
 cd ../..
 #rm objectal.tar.gz
 rm -rf ObjectAL
