5d8af0285fa4ac1d990e1e05c9e6801c0dad5e4d
ibinti <github@ibinti.com>
2016-08-21 04:34:46 -0700

    vm core

--- vm/build.sh
+++ vm/build.sh
@@ -12,14 +12,14 @@ Options:
   --build=[release|debug] Specifies the build type. If not set both release
                           and debug versions of the libraries will be built.
   --target=...            Specifies the target(s) to build for. Supported 
-                          targets are macosx-x86_64, macosx-x86, ios-x86_64,
-                          ios-x86, ios-thumbv7, ios-arm64, linux-x86_64, 
-                          linux-x86. Enclose multiple targets in quotes and 
+                          targets are macosx-x86_64, ios-x86_64,
+                          ios-x86, ios-thumbv7, ios-arm64, linux-x86_64.
+                          Enclose multiple targets in quotes and
                           separate with spaces or specify --target multiple
                           times. If not set the current host OS determines the
-                          targets. macosx-x86_64, macosx-x86, ios-x86_64,
+                          targets. macosx-x86_64, ios-x86_64,
                           ios-x86, ios-thumbv7 and ios-arm64 on MacOSX and
-                          linux-x86_64 and linux-x86 on Linux.
+                          linux-x86_64 on Linux.
   --verbose               Enable verbose output during the build.
   --clean                 Cleans the build dir before starting the build.
   --help                  Displays this information and exits.
@@ -50,10 +50,10 @@ if [ "x$TARGETS" = 'x' ]; then
   OS=$(uname)
   case $OS in
   Darwin)
-    TARGETS="ios-thumbv7 ios-arm64 ios-x86_64 ios-x86 macosx-x86_64 macosx-x86"
+    TARGETS="ios-thumbv7 ios-arm64 ios-x86_64 ios-x86 macosx-x86_64"
     ;;
   Linux)
-    TARGETS="linux-x86_64 linux-x86"
+    TARGETS="linux-x86_64"
     ;;
   *)
     echo "Unsupported OS: $OS"
@@ -67,7 +67,7 @@ fi
 
 # Validate targets
 for T in $TARGETS; do
-  if ! [[ $T =~ (macosx-(x86_64|x86))|(ios-(x86_64|x86|thumbv7|arm64))|(linux-(x86_64|x86)) ]] ; then
+  if ! [[ $T =~ (macosx-(x86_64))|(ios-(x86_64|x86|thumbv7|arm64))|(linux-(x86_64)) ]] ; then
     echo "Unsupported target: $T"
     exit 1
   fi
