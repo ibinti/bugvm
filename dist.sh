0bf87425e0321ef1401333455801acb3b5254874
ibinti <inprinciple@gmail.com>
2016-11-03 09:17:42 -0700

    clean

--- dist.sh
+++ dist.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#./gradlew clean build dist dev env publishToMavenLocal copyIdea newMvn runMvn
+#./gradlew clean build dist dev env publishToMavenLocal copyIdea
 ./gradlew clean build copyIdea env publishToMavenLocal
