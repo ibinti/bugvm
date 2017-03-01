d86311158bcb9aff766f2f9eed9306097e7e587a
ibinti <inprinciple@gmail.com>
2017-03-01 10:27:46 -0800

    syntax

--- rt/vm/build.sh
+++ rt/vm/build.sh
@@ -74,7 +74,7 @@ if [ "x$TARGETS" = 'x' ]; then
   OS=$(uname)
   case $OS in
   Darwin)
-    if [ $MACOSX_SDK_VERSION -eq 10.7 ] ; then
+    if [ $MACOSX_SDK_VERSION = 10.7 ] ; then
       TARGETS="ios-thumbv7 ios-x86_64 ios-x86 macosx-x86_64"
     else
       TARGETS="ios-thumbv7 ios-arm64 ios-x86_64 ios-x86 macosx-x86_64"
