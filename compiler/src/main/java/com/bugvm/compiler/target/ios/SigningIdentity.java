47eadacf2241db9fd4ebb597099f7c34f6b22c47
ibinti <inprinciple@gmail.com>
2017-04-07 15:31:07 -0700

    Playing With Kotlin

--- compiler/src/main/java/com/bugvm/compiler/target/ios/SigningIdentity.java
+++ compiler/src/main/java/com/bugvm/compiler/target/ios/SigningIdentity.java
@@ -33,29 +33,29 @@ public class SigningIdentity implements Comparable<SigningIdentity> {
 
     private final String name;
     private final String fingerprint;
-    
+
     SigningIdentity(String name, String fingerprint) {
         this.name = name;
         this.fingerprint = fingerprint;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public String getFingerprint() {
         return fingerprint;
     }
-    
+
     @Override
     public int compareTo(SigningIdentity o) {
         return this.name.compareToIgnoreCase(o.name);
     }
-    
+
     @Override
     public String toString() {
         return "SigningIdentity [name=" + name + ", fingerprint=" + fingerprint
-                + "]";
+        + "]";
     }
 
     public static SigningIdentity find(List<SigningIdentity> ids, String search) {
@@ -75,7 +75,7 @@ public class SigningIdentity implements Comparable<SigningIdentity> {
         }
         throw new IllegalArgumentException("No signing identity found matching '" + search + "'");
     }
-    
+
     protected static List<SigningIdentity> parse(String securityOutput) {
         /* Output from security looks like this:
          *   [... ommitted output ...]
@@ -88,19 +88,19 @@ public class SigningIdentity implements Comparable<SigningIdentity> {
         ArrayList<SigningIdentity> ids = new ArrayList<SigningIdentity>();
         Pattern pattern = Pattern.compile("^\\d+\\)\\s+([0-9A-F]+)\\s+\"([^\"]*)\"\\s*(.*)");
         for (String line : securityOutput.split("\n")) {
-            line = line.trim();
-            Matcher matcher = pattern.matcher(line);
-            if (!matcher.find()) {
-                continue;
-            }
-            String name = matcher.group(2);
-            String fingerprint = matcher.group(1);
-            String flags = matcher.group(3);
-            // See cssmerr.h for possible CSSMERR_TP_CERT_* constants.
-            if (flags == null || !flags.contains("CSSMERR_TP_CERT_")) {
-                ids.add(new SigningIdentity(name, fingerprint));
-            }
+        line = line.trim();
+        Matcher matcher = pattern.matcher(line);
+        if (!matcher.find()) {
+            continue;
         }
+        String name = matcher.group(2);
+        String fingerprint = matcher.group(1);
+        String flags = matcher.group(3);
+        // See cssmerr.h for possible CSSMERR_TP_CERT_* constants.
+        if (flags == null || !flags.contains("CSSMERR_TP_CERT_")) {
+            ids.add(new SigningIdentity(name, fingerprint));
+        }
+    }
         Collections.sort(ids);
         return ids;
     }
@@ -108,12 +108,12 @@ public class SigningIdentity implements Comparable<SigningIdentity> {
     public static List<SigningIdentity> list() {
         try {
             return parse(new Executor(Logger.NULL_LOGGER, "security")
-                .args("find-identity", "-v", "-p", "codesigning").execCapture());
+                    .args("find-identity", "-v", "-p", "codesigning").execCapture());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    
+
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println(list());
