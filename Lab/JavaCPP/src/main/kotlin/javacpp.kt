a360011593d3b96fdd69c269528d0995be033a79
ibinti <inprinciple@gmail.com>
2017-06-13 00:55:28 -0700

    lab

---
+++ Lab/JavaCPP/src/main/kotlin/javacpp.kt
@@ -0,0 +1,13 @@
+class javacpp {
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            println("Running javacpp!")
+            for( a in args) {
+                println(a)
+            }
+            //org.bytedeco.javacpp.tools.Builder.main(args)
+        }
+    }
+
+}
